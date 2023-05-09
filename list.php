1d98857a54535edd627527ad32ce269ecdb6b0e3
JeniferSant22 <125519506+JeniferSant22@users.noreply.github.com>
2023-05-09 19:56:21 -0300

    Update list.php

--- list.php
+++ list.php
@@ -1,38 +1,51 @@
 <?php
+
 require_once './conn.php';
 
-$stmt = $conn -> prepare("SELECT * FROM users");
-$stmt-> execute();
-$list = $stmt-> fetchAll(PDO::FETCH_ASSOC);
+//preparar
+$stmt = $conn->prepare("SELECT * FROM users");
+
+//executar
+$stmt->execute();
+
+//listar
+$list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//var_dump($list);
+
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista de Clientes</title>
 </head>
 <body>
-    <table border ="1">
+    <table border="1">
         <tr>
-            <th>ID</th> 
-            <th>Nome</th> 
-            <th>E-mail</th> 
-            <th>Ações</th> 
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Endereço</th>
+            <th>Data de Anivesário</th>
         </tr>
-        <?php foreach ($list as $user): ?>
+            <?php foreach ($list as $row): ?>
         <tr>
-            <td><?= $user['id']; ?></td>        
-            <td><?= $user['name'];?></td>        
-            <td><?= $user['email'];?></td>        
+            <td><?= $row['id']; ?></td>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['email']; ?></td>
+            <td><?= $row['address']; ?></td>
+            <td><?= $row['birthday']; ?></td>
             <td>
-                <a href="update.php?id=<?= $user['id']; ?>">Editar </a>
-            
-                <a href="delete.php?id=<?= $user['id']; ?>">Excluir </a>
-            </td>   
+                <a href="update.php?id=<?= $row['id']; ?>">Editar</a>
+                |
+                <a href="delete.php?id=<?= $row['id']; ?>">Deletar</a>
+            </td>
         </tr>
         <?php endforeach; ?>
-    </table>         
+    </table>
 </body>
 </html>
